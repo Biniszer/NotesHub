60495cb1aa5be8297b65aa8a21a29eb0b7243fb4
Biniszer <kbiniszewski86@gmail.com>
2024-01-21 16:42:14 +0100

    Add project files.

---
+++ NotesHub/Entities/Note.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotesHub.Entities
+{
+    public class Note
+    {
+        public Guid Id { get; set; }
+        public string State { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public string Tags{ get; set; }
+        public DateTime CreationTime { get; set; }
+        public string Comments {  get; set; }
+        public string Content { get; set; }
+    }
+}
---
+++ NotesHub/Entities/NotesHubContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NotesHub.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace NotesHub.Entities
+{
+    public class NotesHubContext : DbContext
+    {
+        public DbSet<Note> Notes { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        private const string ConnectionstringKey = "postgres";
+        private const string JsonFile = "appsettings.json";
+        public static AppSettingsJson ReadConfiguration()
+        {
+            string json = File.ReadAllText(JsonFile);
+            return JsonSerializer.Deserialize<AppSettingsJson>(json);
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var configuration= ReadConfiguration();
+            var connectionString = configuration.ConnectionString[ConnectionstringKey];
+            optionsBuilder.UseNpgsql(connectionString);
+        }
+    }
+}
